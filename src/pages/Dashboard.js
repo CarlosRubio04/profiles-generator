e544e7765cb9e1cf61a1e76b8c1c4c8f5378adb2
Carlos Rubio <crubio@acsendo.com>
2019-05-16 15:45:47 -0500

    Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 export default class Dashboard extends Component {
     render() {
         return (
-            'Hola'
+            <section className="Main">
+                <div className="Main-Container">
+                    <div className="Row">
+
+                    </div>
+                </div>
+            </section>
         )
     }
 }
