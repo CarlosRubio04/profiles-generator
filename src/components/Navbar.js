8bf56bf9b8a5ec779c9eb1065997c55371d60815
Carlos Rubio <crubio@acsendo.com>
2019-07-18 08:53:31 -0500

    Agrago metodo de autenticacion con google

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import withFirebaseAuth from "react-with-firebase-auth";
+import "firebase/auth";
+import firebase from "../firebase";
 
-import './styles/Navbar.scss';
+import { Link } from "react-router-dom";
 
+import "./styles/Navbar.scss";
+
+const firebaseAppAuth = firebase.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider()
+};
 
 class Navbar extends React.Component {
-    render() {
-        return (
-            <div className="Navbar Bg-Primary">
-                <div className="Navbar-Container">
-                    <Link to="/" className="Body-1">
-                        Acsendo<span>Profiles</span>
-                    </Link>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { user, signOut, signInWithGoogle } = this.props;
+    return (
+      <div className="Navbar Bg-Primary">
+        <div className="Navbar-Container">
+          <Link to="/" className="Body-1">
+            Acsendo<span>Profiles</span>
+          </Link>
+          <div className="User">
+            {user ? <p className="Body-1">Hello, {user.displayName}</p> : <p className="Body-1">Please sign in</p>}
+            {user ? (
+              <button className="Text-Button Button Button-Secondary-Dark" onClick={signOut}>Sign out</button>
+            ) : (
+              <button className="Text-Button Button Button-Secondary-Dark" onClick={signInWithGoogle}>Sign in with Google</button>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Navbar;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth
+})(Navbar);
